e2f6c46e335e29b63f27aefe9d3f9b297276934d
jwong13 <jason.wong@illumina.com>
2024-04-17 21:36:38 +0800

    fix set issue in redis

--- internal/app/cache.go
+++ internal/app/cache.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -24,15 +25,22 @@ func NewRedisClient(host string) *RedisClient {
 }
 
 func (r *RedisClient) AddPending(id int) error {
-	return r.client.SAdd(r.ctx, "pending", id).Err()
+	idStr := strconv.Itoa(id)
+	// return r.client.SAdd(r.ctx, "pending", idStr).Err()
+	return r.client.Set(r.ctx, idStr, "pending", 60*time.Second).Err()
 }
 
 func (r *RedisClient) IsPending(id int) bool {
-	return r.client.SIsMember(r.ctx, "pending", id).Val()
+	idStr := strconv.Itoa(id)
+	// return r.client.SIsMember(r.ctx, "pending", idStr).Val()
+	val := r.client.Get(r.ctx, idStr).Val()
+	return val == "pending"
 }
 
 func (r *RedisClient) RemovePending(id int) error {
-	return r.client.SRem(r.ctx, "pending", id).Err()
+	idStr := strconv.Itoa(id)
+	// return r.client.SRem(r.ctx, "pending", idStr).Err()
+	return r.client.Del(r.ctx, idStr).Err()
 }
 
 func (r *RedisClient) SetSeq(key string, value interface{}) error {
